1f1ff2d5bf2425b042ef65c8c79748f6dad0adeb
ShivajiBajpai98 <84443265+ShivajiBajpai98@users.noreply.github.com>
2021-10-07 16:39:28 +0530

    Initial commit

---
+++ src/com/company/DuplicateCharacter.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
+
+public class DuplicateCharacter
+{
+    public void findCharacter()
+    {
+        Map<Character, Integer> baseMap = new HashMap<Character, Integer>();
+        DuplicateCharacter m = new DuplicateCharacter();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a word: ");
+        String string =scanner.nextLine();
+        char[] charArray = string.toCharArray();
+        for (Character character : charArray) {
+            if (baseMap.containsKey(character)) {
+                baseMap.put(character, baseMap.get(character) + 1);
+            } else {
+                baseMap.put(character, 1);
+            }
+        }
+        Set<Character> keys = baseMap.keySet();
+        for (Character chars : keys) {
+            if (baseMap.get(chars) > 1) {
+                System.out.println(chars + "  is " + baseMap.get(chars) + " times");
+            }
+        }
+    }
+        public static void main (String[]args)
+        {
+            DuplicateCharacter duplicateCharacter = new DuplicateCharacter();
+            duplicateCharacter.findCharacter();
+
+        }
+    }
+
